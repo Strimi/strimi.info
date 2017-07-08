f5fd114796ad3b418f5289dab9bfa192e514f326
Mowilimi <kamyk.kamil@gmail.com>
2017-07-08 13:46:27 +0200

    uncomment ip

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,6 +11,7 @@ use Symfony\Component\Debug\Debug;
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 
+
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')) || php_sapi_name() === 'cli-server')
